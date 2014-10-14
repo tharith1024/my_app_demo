292f193eb29a4fb8c459d46a61742df117da3e19
tharith1024 <tharith.than@gmail.com>
2014-10-14 11:05:05 +0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -44,4 +44,15 @@ describe "User pages" do
       end
     end
   end
+
+    describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+  end
 end
